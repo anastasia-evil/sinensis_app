6d76a12dd20491bfa921aaf7214143e94b6650b7
anastasia-evil <100429182@alumnos.uc3m.es>
2023-02-23 12:54:04 +0100

    bb

--- app/src/main/java/com/example/sinensis/activity_datos.java
+++ app/src/main/java/com/example/sinensis/activity_datos.java
@@ -3,12 +3,26 @@ package com.example.sinensis;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class activity_datos extends AppCompatActivity {
 
+    EditText nombre, edad;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_datos);
+        nombre = (EditText) findViewById(R.id.nombre);
+        edad = (EditText) findViewById(R.id.edad);
     }
+
+    public void validacion(View v){
+        if(nombre.getText().toString().isEmpty() || edad.getText().toString().isEmpty()){
+            Toast.makeText(this, "RELLENA LOS CAMPOS CABRON", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+
 }
