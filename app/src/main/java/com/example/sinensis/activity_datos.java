183171f43692010bfd55619b05b34c8380549e3d
anastasia-evil <100429182@alumnos.uc3m.es>
2023-02-27 12:10:48 +0100

    bb

--- app/src/main/java/com/example/sinensis/activity_datos.java
+++ app/src/main/java/com/example/sinensis/activity_datos.java
@@ -5,28 +5,65 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.SeekBar;
 import android.widget.Toast;
 
 public class activity_datos extends AppCompatActivity {
 
     EditText nombre, edad;
+    SeekBar seekbar;
+    int grado;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_datos);
         nombre = (EditText) findViewById(R.id.nombre);
         edad = (EditText) findViewById(R.id.edad);
+        seekbar = (SeekBar) findViewById(R.id.seekbar);
+
+
+
+
+        seekbar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener(){
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser){
+                grado = seekbar.getProgress();
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+
+
+        });
+
+
     }
+
+
+
 
 
 
     public void validar(View v){
         if(nombre.getText().toString().isEmpty() || edad.getText().toString().isEmpty()){
-            Toast toast = Toast.makeText(this, "Rellena todos los campos", Toast.LENGTH_SHORT);
+            Toast toast = Toast.makeText(this, "Rellena todos los campos" + grado, Toast.LENGTH_SHORT);
             toast.show();
         }
 
+
     }
+
+
+
+
+
 
 
 
