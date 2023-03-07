3c4adbd594f2948ac5b365c946df35f09103d57e
anastasia-evil <100429182@alumnos.uc3m.es>
2023-03-07 12:22:35 +0100

    bb

--- app/src/main/java/com/example/sinensis/activity_principal.java
+++ app/src/main/java/com/example/sinensis/activity_principal.java
@@ -1,9 +1,12 @@
 package com.example.sinensis;
 
+import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-
+import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 
 import java.util.List;
@@ -12,29 +15,31 @@ public class activity_principal extends AppCompatActivity {
 
     private AppDatabase db; // una variable vacia donde podremos meter las actividades
 
-    private ListView listView; //lista para visualizar
+    private ListView listActividades; //lista de actividades
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_principal);
 
-        db = AppDatabase.getInstance(getApplicationContext()); // le damos valores de inicio a db
+        db = AppDatabase.getInstance(getApplicationContext()); // le damos valos de inicio a db
 
-        listView = findViewById(R.id.list_view);
-        listView.setOnItemClickListener((adapter, view, pos, id) -> {
+        listActividades = findViewById(R.id.list_view);
+        listActividades.setOnItemClickListener((adapter, view, pos, id) -> {
             Actividades actividades = (Actividades) adapter.getAdapter().getItem(pos);
         });
 
         displayList();
 
+
+
     }
 
     private void displayList() {
-        List<Actividades> actividadesList = db.ActividadesDAO().selectAll();
-        ArrayAdapter<Actividades> listAdapter = new ArrayAdapter<>(listView.getContext(),
-                android.R.layout.simple_list_item_1, actividadesList);
-        listView.setAdapter(listAdapter);
+        List<Actividades> notesList = db.ActividadesDAO().selectAll();
+        ArrayAdapter<Actividades> listAdapter = new ArrayAdapter<>(listActividades.getContext(),
+                android.R.layout.simple_list_item_1, notesList);
+        listActividades.setAdapter(listAdapter);
     }
 
 
