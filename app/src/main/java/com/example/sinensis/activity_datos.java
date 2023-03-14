35fdc64486ee7f4e24b3a4b6991e3bbe8c1c6736
Jima <100429169@alumnos.uc3m.es>
2023-03-14 11:09:53 +0100

    Mapa (Google API Key)

--- app/src/main/java/com/example/sinensis/activity_datos.java
+++ app/src/main/java/com/example/sinensis/activity_datos.java
@@ -1,16 +1,15 @@
 package com.example.sinensis;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.SeekBar;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class activity_datos extends AppCompatActivity {
 
@@ -57,19 +56,26 @@ public class activity_datos extends AppCompatActivity {
 
 
     public void validar(View v){
-        if(nombre.getText().toString().isEmpty() || edad.getText().toString().isEmpty()){
-            Toast toast = Toast.makeText(this, "Rellena todos los campos " + grado, Toast.LENGTH_SHORT);
+        if(nombre.getText().toString().isEmpty()){
+            Toast toast = Toast.makeText(this, "Rellena tu nombre por favor " + grado, Toast.LENGTH_SHORT);
             toast.show();
         }
         else{
-            btn.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    Intent intent = new Intent(activity_datos.this, activity_mentores.class);
-                    startActivity(intent);
-                }
+            if(edad.getText().toString().isEmpty()){
+                Toast toast = Toast.makeText(this, "Rellena tu edad por favor " + grado, Toast.LENGTH_SHORT);
+                toast.show();
+            }
+            else{
+                btn.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        Intent intent = new Intent(activity_datos.this, activity_mentores.class);
+                        startActivity(intent);
+                    }
 
-            });
+                });
+            }
+
         }
 
 
---
+++ app/src/main/java/com/example/sinensis/activity_mapa.java
@@ -0,0 +1,35 @@
+package com.example.sinensis;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class activity_mapa extends AppCompatActivity implements OnMapReadyCallback {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mapa);
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        LatLng madrid = new LatLng(40.416, -3.702);
+        googleMap.addMarker(new MarkerOptions()
+                .position(madrid)
+                .title("Marcador en Madrid"));
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(madrid));
+    }
+
+}
