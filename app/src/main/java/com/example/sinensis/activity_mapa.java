eac129578045133f8c3806eabdc182fa1bf75ab0
Jima <100429169@alumnos.uc3m.es>
2023-04-17 18:29:38 +0200

    Mapa con restaurantes en sol

--- app/src/main/java/com/example/sinensis/activity_mapa.java
+++ app/src/main/java/com/example/sinensis/activity_mapa.java
@@ -1,216 +1,147 @@
 package com.example.sinensis;
 
 import android.Manifest;
+import android.annotation.SuppressLint;
 import android.content.pm.PackageManager;
-import android.location.Address;
-import android.location.Geocoder;
 import android.location.Location;
-import android.location.LocationListener;
-import android.location.LocationManager;
-import android.os.Build;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
-import androidx.appcompat.widget.SearchView;
 import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.FragmentActivity;
 
 import com.example.sinensis.databinding.ActivityMapaBinding;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.Priority;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 
-import java.io.IOException;
-import java.util.List;
-
-public class activity_mapa extends FragmentActivity implements OnMapReadyCallback, View.OnClickListener {
+public class activity_mapa extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
     private ActivityMapaBinding binding;
-    private LocationManager locationManager;
-    private LocationListener locationListener;
-    private Location userCurrentLocation;
+    private FusedLocationProviderClient fusedLocationProviderClient;
+    private static final int Request_code = 101;
+    double lat, lng;
 
-    private SearchView searchView;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
+
         binding = ActivityMapaBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        searchView = findViewById(R.id.idSearchView);
+        fusedLocationProviderClient =
+                LocationServices.getFusedLocationProviderClient(this.getApplicationContext());
 
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
 
-        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
-            @Override
-            public boolean onQueryTextSubmit(String query) {
-                // on below line we are getting the
-                // location name from search view.
-                String location = searchView.getQuery().toString();
+        StringBuilder stringBuilder = new StringBuilder
+                ("https://maps.googleapis.com/maps/api/place/nearbysearch/json?");
+        stringBuilder.append("location=" + lat + ","+ lng);
+        stringBuilder.append("&radius=1000");
+        //CAMBIAR
+        stringBuilder.append("&type=food");
+        stringBuilder.append("&sensor=true");
+        stringBuilder.append("&key=" + getResources().getString(R.string.google_api_key));
 
-                // below line is to create a list of address
-                // where we will store the list of all address.
-                List<Address> addressList = null;
+        String url = stringBuilder.toString();
+        Object dataFetch[] = new Object[2];
+        dataFetch[0] = mMap;
+        dataFetch[1] = url;
 
-                // checking if the entered location is null or not.
-                if (location != null || location.equals("")) {
-                    // on below line we are creating and initializing a geo coder.
-                    Geocoder geocoder = new Geocoder(activity_mapa.this);
-                    try {
-                        // on below line we are getting location from the
-                        // location name and adding that location to address list.
-                        addressList = geocoder.getFromLocationName(location, 1);
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                    // on below line we are getting the location
-                    // from our list a first position.
-                    Address address = addressList.get(0);
-
-                    // on below line we are creating a variable for our location
-                    // where we will add our locations latitude and longitude.
-                    LatLng latLng = new LatLng(address.getLatitude(), address.getLongitude());
-
-                    // on below line we are adding marker to that position.
-                    mMap.addMarker(new MarkerOptions().position(latLng).title(location));
-
-                    // below line is to animate camera to that position.
-                    mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 10));
-                }
-                return false;
-            }
-
-            @Override
-            public boolean onQueryTextChange(String newText) {
-                return false;
-            }
-        });
-        binding.imgZoomOut.setOnClickListener(this);
-        binding.imgZoomIn.setOnClickListener(this);
-        binding.btnLocation.setOnClickListener(this);
-        locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
-        initLocationListener();
-        getUserCurrentLocation();
-        setMarkerAtmyLocation();
-
+        FetchData fetchData = new FetchData();
+        fetchData.execute(dataFetch);
 
     }
 
-    private void getUserCurrentLocation() {
-        if (isPermissionGranted()) {
-            if (locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+    @Override
+    public void onMapReady(@NonNull GoogleMap googleMap) {
+        mMap = googleMap;
 
-                Log.v("method_called","here");
-                if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-                    isPermissionGranted();
+        getCurrentLocation();
+    }
+
+    private void getCurrentLocation(){
+        if(ActivityCompat.checkSelfPermission(
+            this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+            && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED){
+
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, Request_code);
+            return;
+        }
+
+        LocationRequest locationRequest = LocationRequest.create();
+        locationRequest.setInterval(60000);
+        locationRequest.setPriority(Priority.PRIORITY_HIGH_ACCURACY);
+        locationRequest.setFastestInterval(5000);
+        LocationCallback locationCallback = new LocationCallback() {
+            @Override
+            public void onLocationResult(@NonNull LocationResult locationResult) {
+                super.onLocationResult(locationResult);
+                Toast.makeText(getApplicationContext(), "location result is: " + locationResult, Toast.LENGTH_SHORT).show();
+
+                if (locationResult == null){
+                    Toast.makeText(getApplicationContext(), "current location is null", Toast.LENGTH_SHORT).show();
                     return;
                 }
-                locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
 
-            } else if (locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER)) {
-                locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, locationListener);
-            } else {
-                userCurrentLocation=locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
-            }
-        }
-        if (userCurrentLocation!=null) {
-            Log.v("lat_long", "lat:" + userCurrentLocation.getLatitude() + ",longi:" + userCurrentLocation.getLongitude());
-            binding.edtLocation.setText("lat:" + userCurrentLocation.getLatitude() + ",longi:" + userCurrentLocation.getLongitude());
-        }
-    }
-    private boolean isPermissionGranted() {
-        Boolean permissionGranted=true;
-        String[] permissions=new String[]{
-                Manifest.permission.ACCESS_COARSE_LOCATION,
-                Manifest.permission.ACCESS_FINE_LOCATION
-        };
-        if (ActivityCompat.checkSelfPermission(this, permissions[0])!= PackageManager.PERMISSION_GRANTED&&
-                ActivityCompat.checkSelfPermission(this, permissions[1])!= PackageManager.PERMISSION_GRANTED
-        ){
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-                requestPermissions(permissions,123);
-            }
-        }
-        return permissionGranted;
-    }
+                for(Location location:locationResult.getLocations()){
+                    if(location != null){
+                        Toast.makeText(getApplicationContext(), "current location is: " + location.getLongitude(), Toast.LENGTH_SHORT).show();
+                    }
 
-    private void initLocationListener() {
-        locationListener = new LocationListener() {
-            @Override
-            public void onLocationChanged(@NonNull Location location) {
-                userCurrentLocation=location;
-            }
-
-            @Override
-            public void onProviderEnabled(@NonNull String provider) {
-                LocationListener.super.onProviderEnabled(provider);
-                Toast.makeText(activity_mapa.this, "Provider Enabled", Toast.LENGTH_SHORT).show();
-            }
-
-            @Override
-            public void onProviderDisabled(@NonNull String provider) {
-                LocationListener.super.onProviderDisabled(provider);
-                Toast.makeText(activity_mapa.this, "Provider disable", Toast.LENGTH_SHORT).show();
-            }
-
-            @Override
-            public void onStatusChanged(String provider, int status, Bundle extras) {
-                LocationListener.super.onStatusChanged(provider, status, extras);
-                Toast.makeText(activity_mapa.this, "Status changed", Toast.LENGTH_SHORT).show();
+                }
             }
         };
-    }
 
-    @Override
-    public void onMapReady(GoogleMap googleMap) {
-        mMap = googleMap;
-        getUserCurrentLocation();
-        setMarkerAtmyLocation();
-    }
-    private void setMarkerAtmyLocation(){
-        if (userCurrentLocation!=null){
-            Log.v("curent_lat_long","lat:"+userCurrentLocation.getLatitude()+",longitute:"+userCurrentLocation.getLongitude());
-            mMap.clear();
-            Geocoder geocoder=new Geocoder(this);
-            String userAddress="";
-            try {
-                List<Address>  addressList=geocoder.getFromLocation(userCurrentLocation.getLatitude(),userCurrentLocation.getLongitude(),5);
-                Address address=addressList.get(0);
-                userAddress=address.getLocality()+","+address.getAdminArea()+","+address.getCountryName()+","+address.getPostalCode();
-                binding.edtLocation.setText(""+userAddress);
+        fusedLocationProviderClient.requestLocationUpdates(locationRequest, locationCallback, null);
+        Task<Location> task = fusedLocationProviderClient.getLastLocation();
+        task.addOnSuccessListener(new OnSuccessListener<Location>() {
+            @Override
+            public void onSuccess(Location location) {
 
-            } catch (IOException e) {
-                e.printStackTrace();
+                if(location != null){
+
+                    lat = location.getLatitude();
+                    lng = location.getLongitude();
+
+                    LatLng latLng = new LatLng(lat, lng);
+                    mMap.addMarker(new MarkerOptions().position(latLng).title("current location!"));
+                    mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
+                    mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, 15));
+
+                }
+
             }
-            LatLng myLocation = new LatLng(userCurrentLocation.getLatitude(), userCurrentLocation.getLongitude());
-            mMap.addMarker(new MarkerOptions().position(myLocation).title(""+userAddress));
-            mMap.moveCamera(CameraUpdateFactory.newLatLng(myLocation));
-        }
+        });
     }
 
+    @SuppressLint("MissingSuperCall")
     @Override
-    public void onClick(View view) {
-        Log.i("click_event","called");
-        switch (view.getId()){
-            case R.id.imgZoomIn:
-                mMap.animateCamera(CameraUpdateFactory.zoomIn());
-                break;
-            case R.id.imgZoomOut:
-                mMap.animateCamera(CameraUpdateFactory.zoomOut());
-                break;
-            case R.id.btnLocation:
-                getUserCurrentLocation();
-                setMarkerAtmyLocation();
-                break;
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+
+        switch(Request_code){
+
+            case Request_code:
+                if(grantResults.length> 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                    getCurrentLocation();
+                }
+
         }
+
     }
 }
