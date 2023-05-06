a1e700e4b18b4faa430e73bf184ac539dc7eba1e
Jima <100429169@alumnos.uc3m.es>
2023-05-06 18:51:40 +0200

    Codigo mapa limpio y comentado

--- app/src/main/java/com/example/sinensis/activity_mapa.java
+++ app/src/main/java/com/example/sinensis/activity_mapa.java
@@ -32,30 +32,37 @@ import com.google.android.gms.tasks.Task;
 
 public class activity_mapa extends FragmentActivity implements OnMapReadyCallback, View.OnClickListener {
 
-    private GoogleMap mMap;
-    private ActivityMapaBinding binding;
-    private FusedLocationProviderClient fusedLocationProviderClient;
-    private static final int Request_code = 101;
-    private double lat, lng;
+    private GoogleMap mMap; // Atributo del mapa
+    private ActivityMapaBinding binding;    // Vínculo/enlace de esta actividad con la interfaz de usuario
+    private FusedLocationProviderClient fusedLocationProviderClient;    // Para obtener la ubicación del dispositivo móvil.
+    private static final int Request_code = 101;    // Para identificar una solicitud específica en onRequestPermissionsResult()
+    private double lat, lng;    // Para guardar la localización de usuario
 
 
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
 
-        binding = ActivityMapaBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
+        binding = ActivityMapaBinding.inflate(getLayoutInflater());     // Inflamos el diseño de la interfaz de usuario
+        setContentView(binding.getRoot());      // Establecer el diseño de la interfaz de usuario a partir de la vista raíz de Data Binding
+
+        // Obtenemos una instancia de Fused Location Provider
 
         fusedLocationProviderClient =
                 LocationServices.getFusedLocationProviderClient(this.getApplicationContext());
+
+        // Obtenemos una instancia del fragmento de mapa de Google Maps y configuramos su visibilidad
 
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         assert mapFragment != null;
         mapFragment.getMapAsync(this);
 
+        // Establecemos escuchadores de clic en dos vistas de la lupa de zoom con Data Binding
         binding.imgZoomOut.setOnClickListener(this);
         binding.imgZoomIn.setOnClickListener(this);
 
+        // Buscamos la referencia del text view de la info de la búsqueda, y seteamos el contenido según
+        // la búsqueda que sea correspondiente
         TextView busqueda = (TextView) findViewById(R.id.busqueda);
 
         if (activity_actividadLista.act_places == 0){
@@ -66,6 +73,7 @@ public class activity_mapa extends FragmentActivity implements OnMapReadyCallbac
 
     }
 
+    // Construimos la URL que se utilizará para hacer una solicitud a la API de Google Places y recuperar lugares cercanos
     public void buscarSitiosCercanos(String sitio){
 
         String url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location="+ lat + "," + lng +
@@ -79,11 +87,22 @@ public class activity_mapa extends FragmentActivity implements OnMapReadyCallbac
         fetchData.execute(dataFetch);
     }
 
+    // El objeto mMap se inicializa con el objeto googleMap que se recibe como parámetro.
+    // Luego, se llama al método getCurrentLocation() para obtener la ubicación actual del
+    // dispositivo y mostrarla en el mapa
+
     @Override
     public void onMapReady(@NonNull GoogleMap googleMap) {
         mMap = googleMap;
         getCurrentLocation();
     }
+
+
+    // Comprobamos si la aplicación tiene permiso para acceder a la ubicación del dispositivo.
+    // Si el permiso no está otorgado, se solicita al usuario que lo permita.
+    // Creamos un objeto LocationRequest que se utiliza para solicitar actualizaciones de ubicación.
+    // Después creamos un objeto LocationCallback que se utiliza para recibir actualizaciones de ubicación.
+    // Finalmente realizamos la búsqueda de sitios dependiendo del tipo de destino correspondiente.
 
     private void getCurrentLocation(){
         if(ActivityCompat.checkSelfPermission(
@@ -156,6 +175,7 @@ public class activity_mapa extends FragmentActivity implements OnMapReadyCallbac
         });
     }
 
+    // Método de devolución de llamada que se llama cuando el usuario responde a la solicitud de permiso
     @SuppressLint("MissingSuperCall")
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
@@ -170,6 +190,9 @@ public class activity_mapa extends FragmentActivity implements OnMapReadyCallbac
         }
 
     }
+
+    // Método que implementa la interfaz View.OnClickListener para manejar los eventos de
+    // click en dos botones de lupa, imgZoomIn e imgZoomOut.
 
     public void onClick(View view) {
         Log.i("click_event","called");
